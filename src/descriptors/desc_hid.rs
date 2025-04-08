f7700b877b9897aec3c015909af7100608a72c33
dbydd <1992003927@qq.com>
2025-04-08 16:14:00 +0800

    add more util

--- src/descriptors/desc_hid.rs
+++ src/descriptors/desc_hid.rs
@@ -46,8 +46,8 @@ pub enum USBHIDProtocolDescriptorType {
 pub enum HIDDescriptorTypes {
     //HID
     Hid = 0x21,
-    // HIDReport = 0x22,
-    // HIDPhysical = 0x23,
+    HIDReport = 0x22, //ONLY FOR DRIVERS. if you wanna decode hid report, please use axhid crate
+    HIDPhysical = 0x23,
 }
 #[derive(Debug)]
 pub struct ReportEvent {
@@ -56,23 +56,3 @@ pub struct ReportEvent {
     pub value: i32,
     pub relative: bool,
 }
-
-// #[derive(Debug)]
-// pub struct ReportInput {
-//     pub bit_length: usize,
-//     pub bit_offset: usize,
-//     pub global_state: GlobalItemsState,
-//     pub local_state: LocalItemsState,
-//     pub flags: MainItemFlags,
-// }
-
-// pub struct ReportHandler {
-//     pub inputs: Vec<ReportInput>,
-//     pub total_byte_length: usize,
-//     pub absolutes: HashMap<(u32, u32), i32>,
-//     pub arrays: HashSet<(u32, u32)>,
-// }
-
-// impl ReportHandler {
-//     pub fn new() -> Result<self, Error> {}
-// }
